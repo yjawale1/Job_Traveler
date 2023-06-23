dc89af9e20e820fcbd41cb093ac324a78a8db485
Yash Jawale <yjawale1@binghamton.edu>
2023-06-23 16:08:08 -0400

    Add project files.

---
+++ ASME_Forms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASME_Forms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
