4f0d3f3c2816c0689b22043c6a9ce6c8d9426513
Yash Jawale <yjawale1@binghamton.edu>
2023-06-23 20:14:16 -0400

    Initial

---
+++ ASME_Forms/Models/ASME_CBI.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASME_Forms.Models
+{
+    public class ASME_CBI
+    {
+        [Key]
+        public int JOB_TRAVELER_NUMBER { get; set; }
+        public int NATL_BD_NUMBER { get; set; }
+        public int PART_NUMBER { get; set; }
+        public int BASIC_TANK_NUMBER { get; set; }
+        public int HX_NUMBER { get; set; }
+        public string DESCRIPTION { get; set; }
+        public int DWG_NUMBER { get; set; }
+        public int MVP_NUMBER { get; set; }
+    }
+}
